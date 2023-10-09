55e85b56dedbb397bd39dc1c13eb62dcc69dadfb
Stormspirit03 <Raviraj03.py@gmail.com>
2023-10-09 08:43:28 +0530

    Queue implemntation using array and merging 2 queues

--- merging2Queues.js
+++ merging2Queues.js
@@ -1,7 +1,7 @@
 class Queue{
     constructor(values) {
-        if(!values) this.data = [];
-        this.data = values;
+        if(!values) this.data = []
+        else this.data = values
     }
     enQueue(ele){
         this.data.unshift(ele);
@@ -20,6 +20,40 @@ class Queue{
     }
 }
 
-let q1 = new Queue([1,2,3]);
-let q2 = new Queue(['a','b','c']);
-console.log('q1', q1.data, ' q2:', q2.data);
+let q1 = new Queue();
+let q2 = new Queue();
+let q3 = new Queue();
+q1.enQueue(1);
+q1.enQueue(2);
+q1.enQueue(3);
+q2.enQueue('a');
+q2.enQueue('b');
+q2.enQueue('c');
+// q3.enQueue(5);
+console.log('q1', q1.data, ' q2:', q2.data, ' q3:',q3);
+// output : q1 [ 3, 2, 1 ]  q2: [ 'c', 'b', 'a' ]  q3: Queue { data: [] }
+
+
+while(q1.peek() || q2.peek()){
+    if(q1.peek()){
+        q3.enQueue(q1.deQueue());
+    }
+    if (q2.peek()){
+        q3.enQueue(q2.deQueue());
+    }
+}
+console.log('After----','q1', q1.data, ' q2:', q2.data, ' q3:',q3);
+//output After---- q1 []  q2: []  q3: Queue { data: [ 3, 'c', 2, 'b', 1, 'a' ] }
+while(q3.peek()){
+    console.log(q3.deQueue());
+}
+
+/* 
+output 
+1
+a
+2
+b
+3
+c
+ */
